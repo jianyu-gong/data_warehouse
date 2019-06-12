93dd7215c07699c3479c8a24f876aec966f74783
Jianyu Gong <jianyu0503@gmail.com>
2019-06-12 16:21:27 -0400

    Update sql_queries.py

--- sql_queries.py
+++ sql_queries.py
@@ -8,7 +8,7 @@ config.read('dwh.cfg')
 # DROP TABLES
 
 staging_events_table_drop = "DROP TABLE IF EXISTS staging_events"
-staging_songs_table_drop = "DROP TABLE IF EXISTS songplays"
+staging_songs_table_drop = "DROP TABLE IF EXISTS staging_songs"
 songplay_table_drop = "DROP TABLE IF EXISTS songplays"
 user_table_drop = "DROP TABLE IF EXISTS users"
 song_table_drop = "DROP TABLE IF EXISTS songs"
@@ -23,53 +23,53 @@ CREATE TABLE staging_events(
     artist VARCHAR(255),
     auth VARCHAR(255),
     firstName VARCHAR(255),
-    gender  VARCHAR(255),
-    ItemInSession	INT,
+    gender VARCHAR(1),
+    itemInSession INT,
     lastName VARCHAR(255),
-    length	DOUBLE PRECISION, 
-    level VARCHAR(255),
-    location VARCHAR(255),	
-    method VARCHAR(255),
-    page VARCHAR(255),	
-    registration VARCHAR(255),	
-    session_id	INT,
+    length DOUBLE PRECISION, 
+    level VARCHAR(50),
+    location VARCHAR(255),  
+    method VARCHAR(25),
+    page VARCHAR(35),   
+    registration BIGINT,    
+    session_id BIGINT,
     song VARCHAR(255),
-    status INT,	
-    ts VARCHAR(255),
-    user_agent VARCHAR(255),	
-    user_id INT);
+    status INT, 
+    ts VARCHAR(50),
+    user_agent TEXT,    
+    user_id VARCHAR(100));
 """)
 
 staging_songs_table_create = ("""
 CREATE TABLE staging_songs(
-    song_id VARCHAR(255) PRIMARY KEY,
     num_songs INT,
-    artist_id VARCHAR(255),
+    artist_id VARCHAR(100),
     artist_latitude DOUBLE PRECISION,
     artist_longitude DOUBLE PRECISION,
     artist_location VARCHAR(255),
     artist_name VARCHAR(255),
+    song_id VARCHAR(100) PRIMARY KEY,
     title VARCHAR(255),
     duration DOUBLE PRECISION,
     year INT);
 """)
 
 songplay_table_create = ("""
-CREATE TABLE IF NOT EXISTS songplays ( \
- songplay_id IDENTITY(0,1) PRIMARY KEY, \
- start_time TIMESTAMP NOT NULL, \
- user_id INT NOT NULL, \
- level VARCHAR(255), \
- song_id VARCHAR(255), \
- artist_id VARCHAR(255), \
- session_id INT, \
- location VARCHAR(255), \
+CREATE TABLE IF NOT EXISTS songplays (
+ songplay_id INT IDENTITY(0,1) PRIMARY KEY SORTKEY,
+ start_time TIMESTAMP NOT NULL REFERENCES time(start_time),
+ user_id INT NOT NULL REFERENCES users(user_id),
+ level VARCHAR(255),
+ song_id VARCHAR(255) REFERENCES songs(song_id),
+ artist_id VARCHAR(255) REFERENCES artists(artist_id),
+ session_id INT, 
+ location VARCHAR(255),
  user_agent VARCHAR(255));
 """)
 
 user_table_create = ("""
 CREATE TABLE IF NOT EXISTS users ( \
- user_id INT PRIMARY KEY, \
+ user_id INT PRIMARY KEY SORTKEY, \
  first_name VARCHAR(255), \
  last_name VARCHAR(255), \
  gender VARCHAR(255), \
@@ -78,7 +78,7 @@ CREATE TABLE IF NOT EXISTS users ( \
 
 song_table_create = ("""
 CREATE TABLE IF NOT EXISTS songs ( \
- song_id VARCHAR(255) PRIMARY KEY, \
+ song_id VARCHAR(255) PRIMARY KEY SORTKEY, \
  title VARCHAR(255), \
  artist_id VARCHAR(255), \
  year INT, \
@@ -86,37 +86,51 @@ CREATE TABLE IF NOT EXISTS songs ( \
 """)
 
 artist_table_create = ("""
-CREATE TABLE IF NOT EXISTS artists (
- artist_id VARCHAR(255) PRIMARY KEY, \
+CREATE TABLE IF NOT EXISTS artists ( \
+ artist_id VARCHAR(255) PRIMARY KEY SORTKEY, \
  name VARCHAR(255), \
  location VARCHAR(255), \
  lattitude DOUBLE PRECISION, \
  longitude DOUBLE PRECISION);
 """)
 
-time_table_create = ("""
-CREATE TABLE IF NOT EXISTS time ( \
- start_time TIMESTAMP PRIMARY KEY, \
- hour INT, \
- day INT, \
+time_table_create = ("CREATE TABLE IF NOT EXISTS time (\
+ start_time TIMESTAMP PRIMARY KEY SORTKEY,\
+ hour INT,\
+ day INT,\
  week INT,\
- month INT, \
- year INT, \
- weekday INT);
-""")
+ month INT,\
+ year INT,\
+ weekday INT);")
 
 # STAGING TABLES
 
-staging_events_copy = ("""
+staging_events_copy = ("copy staging_events from {}\
+ credentials 'aws_iam_role={}'\
+ region 'us-west-2' \
+ COMPUPDATE OFF STATUPDATE OFF \
+ JSON {}").format(config.get('S3','LOG_DATA'), config.get('IAM_ROLE', 'ARN'), config.get('S3','LOG_JSONPATH'))
 
-""").format()
-
-staging_songs_copy = ("""
-""").format()
+staging_songs_copy = ("copy staging_songs from {} \
+    credentials 'aws_iam_role={}'\
+    region 'us-west-2'\
+    JSON 'auto'").format(config.get('S3','SONG_DATA'), config.get('IAM_ROLE', 'ARN'))
 
 # FINAL TABLES
 
 songplay_table_insert = ("""
+INSERT INTO songplays (start_time, user_id, level, song_id, artist_id, session_id, location, user_agent) 
+    SELECT TIMESTAMP 'epoch' + ts/1000 *INTERVAL '1 second' as start_time, 
+        e.user_id, 
+        e.level,
+        s.song_id,
+        s.artist_id,
+        e.session_id,
+        e.location,
+        e.user_agent
+    FROM staging_events e, staging_songs s
+    WHERE e.page = 'NextSong'
+    AND e.song_title = s.title
 """)
 
 user_table_insert = ("""
@@ -133,7 +147,7 @@ time_table_insert = ("""
 
 # QUERY LISTS
 
-create_table_queries = [staging_events_table_create, staging_songs_table_create, songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
+create_table_queries = [staging_events_table_create, staging_songs_table_create, user_table_create, song_table_create, artist_table_create, time_table_create, songplay_table_create]
 drop_table_queries = [staging_events_table_drop, staging_songs_table_drop, songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
 copy_table_queries = [staging_events_copy, staging_songs_copy]
 insert_table_queries = [songplay_table_insert, user_table_insert, song_table_insert, artist_table_insert, time_table_insert]
