6d48cf42b33e567f5cf5c60cb5e1eb0f95c3a0a0
Jianyu Gong <jianyu0503@gmail.com>
2019-06-12 21:43:45 -0400

    Update sql_queries.py

--- sql_queries.py
+++ sql_queries.py
@@ -37,7 +37,7 @@ CREATE TABLE staging_events(
     status INT, 
     ts VARCHAR(50),
     user_agent TEXT,    
-    user_id VARCHAR(100));
+    user_id INT);
 """)
 
 staging_songs_table_create = ("""
@@ -112,14 +112,15 @@ staging_events_copy = ("copy staging_events from {}\
  JSON {}").format(config.get('S3','LOG_DATA'), config.get('IAM_ROLE', 'ARN'), config.get('S3','LOG_JSONPATH'))
 
 staging_songs_copy = ("copy staging_songs from {} \
-    credentials 'aws_iam_role={}'\
-    region 'us-west-2'\
-    JSON 'auto'").format(config.get('S3','SONG_DATA'), config.get('IAM_ROLE', 'ARN'))
+ credentials 'aws_iam_role={}'\
+ region 'us-west-2' \
+ COMPUPDATE OFF STATUPDATE OFF \
+ JSON 'auto'").format(config.get('S3','SONG_DATA'), config.get('IAM_ROLE', 'ARN'))
 
 # FINAL TABLES
 
 songplay_table_insert = ("""
-INSERT INTO songplays (start_time, user_id, level, song_id, artist_id, session_id, location, user_agent) 
+    INSERT INTO songplays (start_time, user_id, level, song_id, artist_id, session_id, location, user_agent) 
     SELECT TIMESTAMP 'epoch' + ts/1000 *INTERVAL '1 second' as start_time, 
         e.user_id, 
         e.level,
@@ -134,15 +135,47 @@ INSERT INTO songplays (start_time, user_id, level, song_id, artist_id, session_i
 """)
 
 user_table_insert = ("""
+    INSERT INTO user (user_id, first_name, last_name, gender, level)
+    SELECT user_id,
+           firstName,
+           lastName,
+           gender,
+           level
+    FROM staging_events
+    WHERE page = 'NextSong'
 """)
 
 song_table_insert = ("""
+    INSERT INTO songs (song_id, title, artist_id, year, duration)
+    SELECT song_id, 
+           title,
+           artist_id,
+           year,
+           duration
+    FROM staging_songs
 """)
 
 artist_table_insert = ("""
+    INSERT INTO artists (artist_id, name, location, latitude, longitude)
+    SELECT artist_id,
+           artist_name,
+           artist_location,
+           artist_latitude,
+           artist_longitude
+    FROM staging_songs
 """)
 
 time_table_insert = ("""
+    INSERT INTO time (start_time, hour, day, week, month, year, weekday)
+    SELECT start_time, 
+           EXTRACT(hr from start_time) AS hour,
+           EXTRACT(d from start_time) AS day,
+           EXTRACT(w from start_time) AS week,
+           EXTRACT(mon from start_time) AS month,
+           EXTRACT(yr from start_time) AS year, 
+           EXTRACT(weekday from start_time) AS weekday 
+    FROM ( SELECT DISTINCT  TIMESTAMP 'epoch' + ts/1000 *INTERVAL '1 second' as start_time 
+           FROM staging_events s)
 """)
 
 # QUERY LISTS
